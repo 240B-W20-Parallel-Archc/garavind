d3981b1894e264ce6356427afbaa9bb0b1c58498
gishmit <60870572+gishmit@users.noreply.github.com>
2020-03-27 12:56:35 -0700

    Create doit.sh

---
+++ doit.sh
@@ -0,0 +1,9 @@
+  
+#!/bin/bash
+  
+rm *.csv
+echo "ArraySize(inKB),Thread_Count,Random,Sequential,Read_Modify_Write,CPU_TIME" >> performance_output.csv
+
+make
+./cacheperf -t:1 -r -size:512
+./cacheperf -t:1 -r -size:512 -write 
