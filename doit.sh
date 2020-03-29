ebbb9e268d9aaaba579db83453a75bad7d97fa28
Girish <girish@Girishs-MacBook-Air.local>
2020-03-29 15:23:09 -0700

    Delete Files

--- doit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-  
-rm *.csv
-echo "ArraySize(inKB),Thread_Count,Random,Sequential,RMW,CPU_TIME" >> performance_output.csv
-
-make
-./cacheperf -t:1 -r -size:512
-./cacheperf -t:1 -r -size:512 -RMW 
