4dbc8e99161efe3167466d2e735884dd53789e67
gishmit <60870572+gishmit@users.noreply.github.com>
2020-03-31 18:23:34 -0700

    Update doit.sh

--- doit.sh
+++ doit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
   
 rm *.csv
-echo "ArraySize(inKB),Thread_Count,Random,Sequential,RMW,CPU_TIME" >> performance_output.csv
+echo "Array Size (inKB),Thread Count,Random,Sequential,RMW,CPU TIME(ns)" >> performance_output.csv
 
 make
-./cacheperf -t:1 -r -size:512
-./cacheperf -t:1 -r -size:512 -RMW 
+./cacheperf -t:1 -r -size:8192
+./cacheperf -t:1 -r -size:8192 -RMW 
